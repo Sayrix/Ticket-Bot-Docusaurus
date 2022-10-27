ee4cb6f9fba29b8bd16f2dbbde36593378df2c93
Sayrix <s4yrix+github@gmail.com>
2022-10-27 18:51:30 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'Ticket Bot',
   tagline: 'Dinosaurs are cool',
   url: 'https://ticketbot.js.org',
-  baseUrl: '/ticket-bot-docusaurus/',
+  baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
