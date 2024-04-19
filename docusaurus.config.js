a9b95204a3ebbc383afc3c91d66eec72e65ef88e
Sayrix <43046854+Sayrix@users.noreply.github.com>
2024-04-19 14:51:08 +0200

    🔄 Upgraded to Docusaurus v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,80 +1,80 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const { themes } = require("prism-react-renderer");
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Ticket Bot',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://ticket-bot.pages.dev',
-  baseUrl: '/',
-  trailingSlash: false,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+	title: "Ticket Bot",
+	tagline: "Dinosaurs are cool",
+	url: "https://ticket-bot.pages.dev",
+	baseUrl: "/",
+	trailingSlash: false,
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "img/favicon.ico",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Sayrix', // Usually your GitHub org/user name.
-  projectName: 'ticket-bot-docusaurus', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "Sayrix", // Usually your GitHub org/user name.
+	projectName: "ticket-bot-docusaurus", // Usually your repo name.
 
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internalization, you can use this field to set useful
+	// metadata like html lang. For example, if your site is Chinese, you may want
+	// to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Sayrix/ticket-bot-docusaurus/edit/main/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
+	presets: [
+		[
+			"classic",
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve("./sidebars.js"),
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					editUrl: "https://github.com/Sayrix/ticket-bot-docusaurus/edit/main/",
+				},
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+			}),
+		],
+	],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Ticket Bot',
-        items: [
-          {
-            href: 'https://github.com/Sayrix/ticket-bot',
-            label: 'GitHub',
-            position: 'right',
-          },
-          {
-            href: 'https://discord.gg/VasYV6MEJy',
-            label: 'Discord',
-            position: 'right',
-          },
-        ],
-      },
-      
-      colorMode: {
-        defaultMode: 'dark'
-      },
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				title: "Ticket Bot",
+				items: [
+					{
+						href: "https://github.com/Sayrix/ticket-bot",
+						label: "GitHub",
+						position: "right",
+					},
+					{
+						href: "https://discord.gg/VasYV6MEJy",
+						label: "Discord",
+						position: "right",
+					},
+				],
+			},
 
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+			colorMode: {
+				defaultMode: "dark",
+			},
+
+			prism: {
+				theme: lightTheme,
+				darkTheme: darkTheme,
+			},
+		}),
 };
 
 module.exports = config;
